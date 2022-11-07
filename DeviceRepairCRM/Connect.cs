03399b9fcaa641170e18289b90a7abc22a8faa00
WardyWardy <albert.mingazov.new@bk.ru>
2022-11-07 20:57:10 +0500

    Add connect class

---
+++ DeviceRepairCRM/Connect.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace DeviceRepairCRM
+{
+    class Connect
+    {
+        public SqlConnection Connection = new SqlConnection(@"
+            DataSource = WIN-MKI4LD09KJR\SQLEXPRESS;
+            Initial Catalog = RepairShop;
+            Integrated Security = true;");
+        public SqlDataReader SqlSelect(string command)
+        {
+            SqlCommand cmd = new SqlCommand(command, Connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+        public int SqlEditAddDel(string command)
+        {
+            SqlCommand cmd = new SqlCommand(command, Connection);
+            int a = cmd.ExecuteNonQuery();
+            return a;
+        }
+    }
+}
